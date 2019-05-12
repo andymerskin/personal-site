c70ac14b6799dc77ebd5d4a50ee3b5eeb6fa190c
Andy Merskin <andymerskin@gmail.com>
2019-05-12 17:55:35 -0600

    add SiteHeader

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="container mx-auto">
+  <div id="app">
+    <site-header></site-header>
     <router-view />
   </div>
 </template>
+
+<script>
+import SiteHeader from '@/components/SiteHeader.vue';
+
+export default {
+  components: {
+    SiteHeader
+  }
+}
+</script>
+
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Work+Sans:400,700');
