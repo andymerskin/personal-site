0e97c198b9f79d627d1c5d7e00d35758ec0325c3
Andy Merskin <docmars@gmail.com>
2021-09-21 20:02:01 -0600

    chore: update ContactInfo design, update resume link

--- src/components/ContactInfo.vue
+++ src/components/ContactInfo.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="flex flex-col sm:flex-row sm:justify-center items-center">
+  <div class="flex flex-col sm:flex-row sm:justify-center items-center text-lg">
     <a
       href="mailto:andymerskin@gmail.com"
       target="_blank"
       title="Email Andy Merskin"
       class="hire-me-link bg-blue-500 hover:bg-blue-600 hover:shadow-lg focus:ring text-bg py-4 px-6 rounded-full"><i class="mr-2 far fa-paper-plane"></i>Hire Andy</a>
     <a
-      href="./andymerskin_resume_2019.pdf"
+      href="./andymerskin_resume_2021.pdf"
       target="_blank"
-      class="text-blue-500 hover:underline mt-4 sm:ml-8 sm:mt-0"><i class="mr-2 fas fa-file-alt"></i>View Resume</a>
+      class="text-blue-500 hover:underline mt-4 sm:ml-8 sm:mt-0"><i class="mr-2 fas fa-file-alt"></i>View my resume</a>
   </div>
 </template>
 
