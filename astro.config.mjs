a133f8e939c0ae3a9740b341abc1abe8b79a5201
Andy Merskin <andymerskin@pm.me>
2026-01-23 18:35:46 -0700

    setup netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,24 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import vue from "@astrojs/vue";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), vue()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  redirects: {
+    "/": "/hello",
+    "/work": "/work/public",
+  },
+
+  adapter: netlify({
+    imageCDN: false,
+  }),
+
+  output: "static",
 });
